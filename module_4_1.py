db5b94b27c121a6d2809f87b933120697b1b3d6b
arsenalio <arslanlev@gmail.com>
2024-11-26 12:03:32 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as f1
+import true_math as f2
+result1 = f1.divide(69,3)
+result2 = f1.divide(3,0)
+result3 = f2.divide(49,7)
+result4 = f2.divide(15,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
